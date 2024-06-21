f7669aa8f7764b216d25bbd8976cdefa5f6b2060
sadiomowo-ch <sadiomowo@companieshouse.gov.uk>
2024-06-21 09:21:06 +0100

    Chaning listener path

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -7,7 +7,7 @@ locals {
   container_port             = "8080" # default Java port to match start script
   docker_repo                = "acsp-manage-users-api"
   lb_listener_rule_priority  = 20
-  lb_listener_paths          = ["/acsp-manage-users-api/healthcheck*", "/acsp-manage-users","/acsp-manage?*", "/acsp-manage/companies/*"]
+  lb_listener_paths          = ["/acsp-manage-users-api/healthcheck, /acsp-members/*, /internal/acsp-members/active"]
   healthcheck_path           = "/aacsp-manage-users-api/healthcheck" #healthcheck path for accounts association service
   healthcheck_matcher        = "200"
   application_subnet_ids     = data.aws_subnets.application.ids
