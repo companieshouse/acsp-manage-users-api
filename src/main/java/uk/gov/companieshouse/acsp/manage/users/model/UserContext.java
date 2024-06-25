616962e889fb9be0193fe9650e03bfa217842fb7
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-06-25 13:54:20 +0100

    Base setup

---
+++ src/main/java/uk/gov/companieshouse/acsp/manage/users/model/UserContext.java
@@ -0,0 +1,22 @@
+package uk.gov.companieshouse.acsp.manage.users.model;
+
+import uk.gov.companieshouse.api.accounts.user.model.User;
+
+public final class UserContext {
+
+    private static ThreadLocal<User> userContextThreadLocal;
+
+    public static User getLoggedUser() {
+        return userContextThreadLocal != null ? userContextThreadLocal.get() : null;
+    }
+
+    private UserContext(){}
+    public static void setLoggedUser(final User user) {
+        userContextThreadLocal = new ThreadLocal<>();
+        userContextThreadLocal.set(user);
+    }
+
+    public static void clear(){
+        userContextThreadLocal.remove();
+    }
+}
---
+++ src/main/java/uk/gov/companieshouse/acsp/manage/users/utils/StaticPropertyUtil.java
@@ -0,0 +1,14 @@
+package uk.gov.companieshouse.acsp.manage.users.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StaticPropertyUtil {
+
+    public static String APPLICATION_NAMESPACE;
+
+    public StaticPropertyUtil( @Value("${spring.application.name}") String applicationNameSpace ) {
+        StaticPropertyUtil.APPLICATION_NAMESPACE = applicationNameSpace;
+    }
+}
