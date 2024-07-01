2d88bf9115c951cebc02a3d1e3f538f70a0df7e8
tferns-ch <tferns@companieshouse.gov.uk>
2024-07-01 16:01:27 +0100

    IDVA6-1145: Implement PR comments

--- src/main/java/uk/gov/companieshouse/acsp/manage/users/controller/UserAcspMembership.java
+++ src/main/java/uk/gov/companieshouse/acsp/manage/users/controller/UserAcspMembership.java
@@ -3,11 +3,11 @@ package uk.gov.companieshouse.acsp.manage.users.controller;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import java.util.List;
+import java.util.Objects;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.companieshouse.acsp.manage.users.mapper.AcspMembershipListMapper;
-import uk.gov.companieshouse.acsp.manage.users.model.UserContext;
 import uk.gov.companieshouse.acsp.manage.users.service.AcspMembersService;
 import uk.gov.companieshouse.api.acsp_manage_users.api.UserAcspMembershipInterface;
 import uk.gov.companieshouse.api.acsp_manage_users.model.AcspMembership;
@@ -15,65 +15,42 @@ import uk.gov.companieshouse.api.acsp_manage_users.model.RequestBodyPatch;
 import uk.gov.companieshouse.api.acsp_manage_users.model.RequestBodyPost;
 import uk.gov.companieshouse.api.acsp_manage_users.model.ResponseBodyPost;
 
-import java.util.List;
-
 @RestController
 public class UserAcspMembership implements UserAcspMembershipInterface {
 
-    private final AcspMembershipListMapper acspMembershipListMapper;
-    private final AcspMembersService acspMembersService;
+  private final AcspMembersService acspMembersService;
 
-    public UserAcspMembership(
-            final AcspMembershipListMapper acspMembershipListMapper,
-            final AcspMembersService acspMembersService
-    ) {
-        this.acspMembershipListMapper = acspMembershipListMapper;
-        this.acspMembersService = acspMembersService;
-    }
+  public UserAcspMembership(final AcspMembersService acspMembersService) {
+    this.acspMembersService = acspMembersService;
+  }
 
-    @Override
-    public ResponseEntity<ResponseBodyPost> addAcspMember(
-            @NotNull String xRequestId,
-            @NotNull String ericIdentity,
-            @Valid RequestBodyPost requestBodyPost
-    ) {
-        return null; // TODO(https://companieshouse.atlassian.net/browse/IDVA6-1149)
-    }
+  @Override
+  public ResponseEntity<ResponseBodyPost> addAcspMember(
+      @NotNull String xRequestId,
+      @NotNull String ericIdentity,
+      @Valid RequestBodyPost requestBodyPost) {
+    return null; // TODO(https://companieshouse.atlassian.net/browse/IDVA6-1149)
+  }
 
-    @Override
-    public ResponseEntity<AcspMembership> getAcspMembershipForAcspId(
-            @NotNull String xRequestId,
-            @Pattern(regexp = "^[a-zA-Z0-9]*$") String id
-    ) {
-        return null; // TODO(https://companieshouse.atlassian.net/browse/IDVA6-1146)
-    }
+  @Override
+  public ResponseEntity<AcspMembership> getAcspMembershipForAcspId(
+      @NotNull String xRequestId, @Pattern(regexp = "^[a-zA-Z0-9]*$") String id) {
+    return null; // TODO(https://companieshouse.atlassian.net/browse/IDVA6-1146)
+  }
 
-    @Override
-    public ResponseEntity<List<AcspMembership>> getAcspMembershipForUserId(
-            final String xRequestId,
-            final String ericIdentity,
-            final Boolean includeRemoved
-    ) {
-        final boolean excludeRemoved = includeRemoved == null || !includeRemoved;
+  @Override
+  public ResponseEntity<List<AcspMembership>> getAcspMembershipForUserId(
+      final String xRequestId, final String ericIdentity, final Boolean includeRemoved) {
+    final boolean excludeRemoved = Objects.isNull(includeRemoved) || !includeRemoved;
+    return new ResponseEntity<>(
+        acspMembersService.fetchAcspMemberships(ericIdentity, excludeRemoved), HttpStatus.OK);
+  }
 
-        final List<AcspMembership> memberships = acspMembershipListMapper.daoToDto(
-                acspMembersService.fetchAcspMembers(ericIdentity).filter(
-                        acspMembersDao -> !excludeRemoved || !acspMembersDao.hasBeenRemoved()
-                ).toList(),
-                UserContext.getLoggedUser()
-        );
-
-        return new ResponseEntity<>(memberships, HttpStatus.OK);
-    }
-
-    @Override
-    public ResponseEntity<Void> updateAcspMembershipForId(
-            @NotNull String xRequestId,
-            @Pattern(regexp = "^[a-zA-Z0-9]*$") String id,
-            @Valid RequestBodyPatch requestBodyPatch
-    ) {
-        return null; // TODO(https://companieshouse.atlassian.net/browse/IDVA6-1147)
-    }
-
+  @Override
+  public ResponseEntity<Void> updateAcspMembershipForId(
+      @NotNull String xRequestId,
+      @Pattern(regexp = "^[a-zA-Z0-9]*$") String id,
+      @Valid RequestBodyPatch requestBodyPatch) {
+    return null; // TODO(https://companieshouse.atlassian.net/browse/IDVA6-1147)
+  }
 }
-
--- src/main/java/uk/gov/companieshouse/acsp/manage/users/service/AcspMembersService.java
+++ src/main/java/uk/gov/companieshouse/acsp/manage/users/service/AcspMembersService.java
@@ -1,28 +1,34 @@
 package uk.gov.companieshouse.acsp.manage.users.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import uk.gov.companieshouse.acsp.manage.users.model.AcspMembersDao;
+import uk.gov.companieshouse.acsp.manage.users.mapper.AcspMembershipListMapper;
+import uk.gov.companieshouse.acsp.manage.users.model.UserContext;
 import uk.gov.companieshouse.acsp.manage.users.repositories.AcspMembersRepository;
-
-import java.util.stream.Stream;
+import uk.gov.companieshouse.api.acsp_manage_users.model.AcspMembership;
 
 @Service
 public class AcspMembersService {
 
-    private final AcspMembersRepository acspMembersRepository;
+  private final AcspMembersRepository acspMembersRepository;
+  private final AcspMembershipListMapper acspMembershipListMapper;
 
-    public AcspMembersService(
-            final AcspMembersRepository acspMembersRepository
-    ) {
-        this.acspMembersRepository = acspMembersRepository;
-    }
+  public AcspMembersService(
+      final AcspMembersRepository acspMembersRepository,
+      final AcspMembershipListMapper acspMembershipListMapper) {
+    this.acspMembersRepository = acspMembersRepository;
+    this.acspMembershipListMapper = acspMembershipListMapper;
+  }
 
-    @Transactional(readOnly=true)
-    public Stream<AcspMembersDao> fetchAcspMembers(
-            final String userId
-    ){
-        return acspMembersRepository.fetchAcspMembersByUserId(userId);
-    }
-
+  @Transactional(readOnly = true)
+  public List<AcspMembership> fetchAcspMemberships(
+      final String userId, final boolean excludeRemoved) {
+    return acspMembershipListMapper.daoToDto(
+        acspMembersRepository
+            .fetchAcspMembersByUserId(userId)
+            .filter(acspMembersDao -> !excludeRemoved || !acspMembersDao.hasBeenRemoved())
+            .toList(),
+        UserContext.getLoggedUser());
+  }
 }
