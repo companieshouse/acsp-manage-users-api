85e185d5934e891e1965f6e4257f618e2a3ab855
tferns-ch <tferns@companieshouse.gov.uk>
2024-07-08 10:49:14 +0100

    IDVA6-1212: Implement isActiveMember endpoint and tests
    - Add isActiveMember method in UserAcspMembershipInternal controller
    - Implement fetchActiveAcspMemberByUserIdAndAcspNumber in AcspMembersService
    - Add new query in AcspMembersRepository for fetching active ACSP member
    - Create comprehensive test suite for isActiveMember endpoint in UserAcspMembershipInternalTest
    - Tests cover various scenarios including unauthorized access, inactive ACSP, non-existent user, and active/inactive membership
    - Reorganize existing tests into nested classes for better structure

--- src/main/java/uk/gov/companieshouse/acsp/manage/users/controller/UserAcspMembershipInternal.java
+++ src/main/java/uk/gov/companieshouse/acsp/manage/users/controller/UserAcspMembershipInternal.java
@@ -1,16 +1,22 @@
 package uk.gov.companieshouse.acsp.manage.users.controller;
 
+import static uk.gov.companieshouse.api.acsp_manage_users.model.AcspMembership.AcspStatusEnum.ACTIVE;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.acsp.manage.users.exceptions.BadRequestRuntimeException;
 import uk.gov.companieshouse.acsp.manage.users.exceptions.NotFoundRuntimeException;
+import uk.gov.companieshouse.acsp.manage.users.model.AcspMembersDao;
 import uk.gov.companieshouse.acsp.manage.users.service.AcspDataService;
 import uk.gov.companieshouse.acsp.manage.users.service.AcspMembersService;
 import uk.gov.companieshouse.acsp.manage.users.service.UsersService;
@@ -28,6 +34,9 @@ public class UserAcspMembershipInternal implements UserAcspMembershipInternalInt
 
   private static final Logger LOG =
       LoggerFactory.getLogger(StaticPropertyUtil.APPLICATION_NAMESPACE);
+  private static final String USER_ID_KEY = "userId";
+  private static final String ACSP_NUMBER_KEY = "acspNumber";
+
   private final AcspDataService acspDataService;
   private final UsersService usersService;
   private final AcspMembersService acspMembersService;
@@ -51,7 +60,10 @@ public class UserAcspMembershipInternal implements UserAcspMembershipInternalInt
         xRequestId,
         String.format("Attempting to add ACSP owner for acsp_number %s", acspNumber),
         null);
+
+    // we call this to throw an exception if the acsp data does not exist
     acspDataService.fetchAcspData(acspNumber);
+
     final String userEmail = internalRequestBodyPost.getOwnerEmail();
     final UsersList users =
         Optional.ofNullable(usersService.searchUserDetails(List.of(userEmail)))
@@ -89,11 +101,66 @@ public class UserAcspMembershipInternal implements UserAcspMembershipInternalInt
 
   @Override
   public ResponseEntity<Boolean> isActiveMember(
-      @NotNull String xRequestId,
-      @Pattern(regexp = "^[0-9A-Za-z-_]{0,32}$") String acspNumber,
-      @NotNull @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
-          String userEmail) {
-    return null; // TODO(https://companieshouse.atlassian.net/browse/IDVA6-1212)
+      final String xRequestId, final String acspNumber, final String userEmail) {
+    LOG.infoContext(
+        xRequestId,
+        "Checking if user is an active ACSP member",
+        new HashMap<>(Map.of(ACSP_NUMBER_KEY, acspNumber, "userEmail", userEmail)));
+
+    var acspData = acspDataService.fetchAcspData(acspNumber);
+    if (!ACTIVE.getValue().equals(acspData.getAcspStatus())) {
+      LOG.infoContext(
+          xRequestId,
+          "ACSP is not active",
+          new HashMap<>(
+              Map.of(ACSP_NUMBER_KEY, acspNumber, "acspStatus", acspData.getAcspStatus())));
+      return new ResponseEntity<>(false, HttpStatus.OK);
+    }
+
+    final UsersList users =
+        Optional.ofNullable(usersService.searchUserDetails(List.of(userEmail)))
+            .filter((userList -> !userList.isEmpty()))
+            .orElseThrow(
+                () -> {
+                  LOG.errorContext(
+                      xRequestId,
+                      String.format("Failed to find user with userEmail %s", userEmail),
+                      null,
+                      new HashMap<>(Map.of("userEmail", userEmail)));
+                  return new NotFoundRuntimeException(
+                      StaticPropertyUtil.APPLICATION_NAMESPACE, "Failed to find user");
+                });
+    final String userId = users.getFirst().getUserId();
+
+    LOG.debugContext(
+        xRequestId,
+        "Fetching active ACSP membership for user",
+        new HashMap<>(
+            Map.of(
+                USER_ID_KEY, userId,
+                ACSP_NUMBER_KEY, acspNumber)));
+
+    final Optional<AcspMembersDao> activeMember =
+        acspMembersService.fetchActiveAcspMemberByUserIdAndAcspNumber(userId, acspNumber);
+    if (activeMember.isEmpty()) {
+      LOG.infoContext(
+          xRequestId,
+          "User is not an active member of the ACSP",
+          new HashMap<>(
+              Map.of(
+                  USER_ID_KEY, userId,
+                  ACSP_NUMBER_KEY, acspNumber)));
+      return new ResponseEntity<>(false, HttpStatus.OK);
+    }
+
+    LOG.infoContext(
+        xRequestId,
+        "User is an active member of the ACSP",
+        new HashMap<>(
+            Map.of(
+                USER_ID_KEY, userId,
+                ACSP_NUMBER_KEY, acspNumber)));
+    return new ResponseEntity<>(true, HttpStatus.OK);
   }
 
   @Override
