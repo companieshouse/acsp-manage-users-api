476186b8bfdb22969781b1bd731ec5e43b754056
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-07-11 15:50:22 +0100

    Added extra coverage

---
+++ src/test/java/uk/gov/companieshouse/acsp/manage/users/configuration/ApiClientConfigTest.java
@@ -0,0 +1,16 @@
+package uk.gov.companieshouse.acsp.manage.users.configuration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import uk.gov.companieshouse.api.InternalApiClient;
+
+@Tag("unit-test")
+public class ApiClientConfigTest {
+
+    @Test
+    void internalApiClientIsCorrectType(){
+        Assertions.assertEquals( InternalApiClient.class, new ApiClientConfig().getInternalApiClient().getClass() );
+    }
+
+}
