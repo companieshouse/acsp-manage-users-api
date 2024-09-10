7bf8f9d26c0f318c6f5e5e76ccf175bfc3aeaa1d
krishna-patel-ch <kpatel@CompaniesHouse.gov.uk>
2024-09-10 11:10:13 +0100

    Added tests for ControllerAdvice

---
+++ src/test/java/uk/gov/companieshouse/acsp/manage/users/controller/ControllerAdviceTest.java
@@ -0,0 +1,97 @@
+package uk.gov.companieshouse.acsp.manage.users.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.companieshouse.acsp.manage.users.configuration.InterceptorConfig;
+import uk.gov.companieshouse.acsp.manage.users.exceptions.BadRequestRuntimeException;
+import uk.gov.companieshouse.acsp.manage.users.exceptions.InternalServerErrorRuntimeException;
+import uk.gov.companieshouse.acsp.manage.users.exceptions.NotFoundRuntimeException;
+import uk.gov.companieshouse.acsp.manage.users.service.AcspMembersService;
+import uk.gov.companieshouse.acsp.manage.users.service.UsersService;
+import uk.gov.companieshouse.acsp.manage.users.utils.StaticPropertyUtil;
+
+
+@Tag( "unit-test" )
+@WebMvcTest( AcspMembershipController.class )
+class ControllerAdviceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AcspMembersService acspMemersService;
+
+    @MockBean
+    private InterceptorConfig interceptorConfig;
+
+    @MockBean
+    private UsersService usersService;
+
+    @MockBean
+    private StaticPropertyUtil staticPropertyUtil;
+
+    @BeforeEach
+    void setup() {
+        Mockito.doNothing().when( interceptorConfig ).addInterceptors( any() );
+    }
+
+    @Test
+    void testNotFoundRuntimeError() throws Exception {
+        Mockito.doThrow( new NotFoundRuntimeException( "acsp-manage-users-api", "Couldn't find association" ) ).when( acspMemersService ).fetchMembership( any() );
+
+        mockMvc.perform( get("/acsps/memberships/TS001")
+                        .header( "X-Request-Id", "theId123" )
+                        .header( "ERIC-Identity", "TSU001") )
+                .andExpect( status().isNotFound() );
+    }
+
+    @Test
+    void testBadRequestRuntimeError() throws Exception {
+        Mockito.doThrow( new BadRequestRuntimeException( "Request was less than ideal" ) ).when( acspMemersService ).fetchMembership( any() );
+
+        mockMvc.perform( get("/acsps/memberships/TS001")
+                        .header( "X-Request-Id", "theId123" )
+                        .header( "ERIC-Identity", "TSU001") )
+                .andExpect( status().isBadRequest() );
+    }
+
+    @Test
+    void testConstraintViolationError() throws Exception {
+        mockMvc.perform( get("/acsps/memberships/$$$")
+                        .header( "X-Request-Id", "theId123" )
+                        .header( "ERIC-Identity", "TSU001") )
+                .andExpect( status().isBadRequest() );
+    }
+
+    @Test
+    void testOnInternalServerError() throws Exception {
+        Mockito.doThrow( new NullPointerException( "Something was null, which shouldn't have been." ) ).when( acspMemersService ).fetchMembership( any() );
+
+        mockMvc.perform( get("/acsps/memberships/TS001")
+                        .header( "X-Request-Id", "theId123" )
+                        .header( "ERIC-Identity", "TSU001") )
+                .andExpect( status().isInternalServerError() );
+    }
+
+    @Test
+    void testOnInternalServerErrorRuntimeException() throws Exception {
+        Mockito.doThrow( new InternalServerErrorRuntimeException( "Problem" ) ).when( acspMemersService ).fetchMembership( any() );
+
+        mockMvc.perform( get("/acsps/memberships/TS001")
+                        .header( "X-Request-Id", "theId123" )
+                        .header( "ERIC-Identity", "TSU001") )
+                .andExpect( status().isInternalServerError() );
+    }
+
+}
+
