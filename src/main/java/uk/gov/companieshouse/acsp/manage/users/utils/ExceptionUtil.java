7fdc8c42ea669940a57d78e5aea7cd117e3fc3fd
kpatel-ch <kpatel@companieshouse.gov.uk>
2025-04-08 09:42:56 +0100

    SIV-173: Bug fix - was logging errors when they had not occured

--- src/main/java/uk/gov/companieshouse/acsp/manage/users/utils/ExceptionUtil.java
+++ src/main/java/uk/gov/companieshouse/acsp/manage/users/utils/ExceptionUtil.java
@@ -1,18 +1,19 @@
 package uk.gov.companieshouse.acsp.manage.users.utils;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public final class ExceptionUtil {
 
     private ExceptionUtil(){}
 
-    public static <T, U, P extends Exception, Q extends RuntimeException> Function<T, U> invokeAndMapException( final Function<T, U> method, final Class<P> fromException, final Q toException ){
+    public static <T, U, P extends Exception, Q extends Supplier<RuntimeException>> Function<T, U> invokeAndMapException( final Function<T, U> method, final Class<P> fromException, final Q toException ){
         return input -> {
             try {
                 return method.apply( input );
             } catch ( Exception exception ){
                 if ( fromException.isInstance( exception ) ){
-                    throw toException;
+                    throw toException.get();
                 } else {
                     throw exception;
                 }
